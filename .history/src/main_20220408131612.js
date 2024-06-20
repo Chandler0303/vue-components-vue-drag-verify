d9a2b12b599d3e007914ae15569e76f5edb847a2
Tree <houlingfeng@mail.com>
2024-06-20 15:25:29 +0800

    test

---
+++ .history/src/main_20220408131612.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import App from './App.vue'
+import WalletConnectProvider from '@walletconnect/web3-provider'
+// import Web3 from "web3";
+// const provider = new WalletConnectProvider({
+//     infuraId: "27e484dcd9e3efcfd25a83a78777cdf1",
+// })
+
+// await provider.enable()
+// const web3 = new Web3(provider);
+console.log(WalletConnectProvider)
+// Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
---
+++ .history/src/main_20220408131825.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import App from './App.vue'
+//import WalletConnectProvider from '@walletconnect/web3-provider'
+
+// const provider = new WalletConnectProvider({
+//     infuraId: "27e484dcd9e3efcfd25a83a78777cdf1",
+// })
+
+// await provider.enable()
+// const web3 = new Web3(provider);
+console.log(Web3)
+// Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
